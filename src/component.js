d325667791abc432588b9e3f66f8c8eb355aef4d
shark_wang <longwind.wang@gmail.com>
2015-12-29 10:20:07 +0800

    1st work version

--- src/component.js
+++ src/component.js
@@ -1,12 +1,9 @@
+
 import React from 'react'
 import classnames from 'classnames'
 import autobind from 'autobind-decorator'
 import styles from '../css/component.css'
-import topojson from 'topojson'
-import {Chart, Graticule, Mesh, Polygon, geoPath} from 'react-d3-map-core'
-
-import {projection as projectionFunc} from 'react-d3-map-core'
-
+import Datamap from 'datamaps'
 import d3 from 'd3'
 
 @autobind
@@ -17,95 +14,90 @@ class D3MapComponent extends React.Component {
 
     }
   }
-
   componentDidMount() {
-    // Artificially delay the loading, so the loading state stays visible for a while.
-    // setTimeout(this.loadData, 20000);
-    d3.json("/components/d3map/worldmap.json", this.dataLoaded);
-  }
+		this.drawMap();
+	}
 
-  dataLoaded(err, data) {
-    console.log(err,data);
+	componentWillReceiveProps() {
+		this.clear();
+	}
 
-    if (err) return this.setState({error: err});
-      this.setState({data: data});
-    console.log(data);
-  }
+	componentDidUpdate() {
+		this.drawMap();
+	}
+
+	componentWillUnmount() {
+		this.clear();
+	}
+
+	clear() {
+		const container = this.refs.container;
+
+		for (const child of Array.from(container.childNodes)) {
+			container.removeChild(child);
+		}
+	}
+
+
+	drawMap() {
+		const map = new Datamap(Object.assign({}, { ...this.props }, {
+			element: this.refs.container,
+      geographyConfig: {
+			     dataUrl: '/components/d3map/liangshan.json',
+           highlightBorderColor: '#B7B7B7'
+			},
+			scope: 'liangshan', //important
+      height: this.props.h,
+      width: this.props.w,
+      
+      fills: {
+          defaultFill: '#f0af0a',
+          lt50: 'rgba(0,244,244,0.9)',
+          gt50: 'red'
+      },
+      data: {
+        '071': {fillKey: 'lt50' },
+        '513424': {fillKey: 'gt50' }
+      },
+      setProjection: function(element) {
+         var projection = d3.geo.mercator()
+           .center([102, 27.7])
+           //.rotate([4.4, 0])
+           .scale(6000)
+           .translate([element.offsetWidth / 2, element.offsetHeight / 2])
+
+         console.log(element.offsetWidth,element.offsetHeight)
+         var path = d3.geo.path()
+           .projection(projection);
+
+         return {path: path, projection: projection};
+       }
+
+		}))
+
+		if (this.props.arc) {
+			map.arc(this.props.arc, this.props.arcOptions);
+		}
+
+		if (this.props.bubbles) {
+			map.bubbles(this.props.bubbles, this.props.bubblesOptions);
+		}
+
+		if (this.props.graticule) {
+			map.graticule();
+		}
+
+		if (this.props.labels) {
+			map.labels();
+		}
+	}
+
 
   render() {
-    if (!this.state.data)
-       console.log(this.state.data);
-
-    // if (this.state.error) return React.DOM.div({className: 'message message--error'}, 'Whoops! ' + this.props.url + ' does not exist. Reload the page to try again.');
-    // if (!this.state.data) return React.DOM.div({className: 'message'}, 'Loading ' + this.props.url);
-    // if (!this.state.citydata) return React.DOM.div({className: 'message'}, 'Loading city' + this.props.url);
-    var width = this.props.width,
-    height = this.props.height,
-    margins = {top: 20, right: 50, bottom: 20, left: 50};
-
-    var title = "test chart lib";
-
-    var topodata = this.state.data;
-    console.log(this.state.data);
-
-    //  var dataCountries = topojson.mesh(topodata, topodata.objects.countries, function(a, b) { return a !== b; });
-    // var dataLand = topojson.feature(topodata, topodata.objects.land);
-    // var dataCountries = topojson.feature(topodata, topodata.objects.countries);
-    var scale = (width + 1) / 2 / Math.PI;
-    var translate = [width / 2, height / 2];
-    var precision = .1;
-    var projection = "mercator";
-
-    // var polygonClass = 'polygon-test';
-    //
-    var proj = projectionFunc({
-       projection: 'mercator',
-       scale: scale,
-       translate: translate,
-       precision: precision
-     });
-    var geo = geoPath(proj);
 
     return (
       <div {...this.props}>
-        {
-          this.state.data
-        ? <Chart
-           title= {title}
-           width= {width}
-           height= {height}
-           margins= {margins}
-         >
-           <Graticule
-             projection = {projection}
-             scale= {scale}
-             translate= {translate}
-             precision= {precision}
-             geoPath= {geo}
-           />
-           <Polygon
-             width= {width}
-             height= {height}
-             data= {topojson.feature(topodata, topodata.objects.land)}
-             projection = {projection}
-             scale= {scale}
-             translate= {translate}
-             precision= {precision}
-             geoPath= {geo}
-           />
-           <Mesh
-             width= {width}
-             height= {height}
-             data= {topojson.feature(topodata, topodata.objects.countries)}
-             projection = {projection}
-             scale= {scale}
-             translate= {translate}
-             precision= {precision}
-             geoPath= {geo}
-           />
-         </Chart>
-         :<div >no</div>
-        }
+        <div ref="container" style={styles}></div>
       </div>
     )
 
